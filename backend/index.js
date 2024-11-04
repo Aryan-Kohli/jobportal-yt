80a3cdb5c8c9f048534312a37c371045937a0195
Aryan-Kohli <106698463+Aryan-Kohli@users.noreply.github.com>
2024-11-04 17:04:31 +0530

    error2

--- backend/index.js
+++ backend/index.js
@@ -22,8 +22,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
-    // origin:'https://carrergo.netlify.app',
+    // origin:'http://localhost:5173',
+    origin:'https://carrergo.netlify.app',
     credentials:true
 }
 
